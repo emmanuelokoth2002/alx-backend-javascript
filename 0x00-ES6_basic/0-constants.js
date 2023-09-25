fb70e2a308db3605c99c1c98274040dbea746441
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-09-25 17:53:59 +0000

    Updates

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,5 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.'; // Use const instead of var
+    const task = 'I prefer const when I can.';
     return task;
   }
   
@@ -8,7 +8,7 @@ export function taskFirst() {
   }
   
   export function taskNext() {
-    let combination = 'But sometimes let'; // Use let instead of var
+    let combination = 'But sometimes let';
     combination += getLast();
   
     return combination;
