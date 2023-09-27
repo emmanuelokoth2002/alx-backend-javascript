e4053464296a2ae9f2b06054b41ccd25591e084d
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-09-27 14:36:37 +0000

    Updates

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,16 @@
-export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => ({
-      status: 200,
-      body: 'success',
-    }))
-    .catch(() => new Error())
-    .then(() => {
+function handleResponseFromAPI(promise) {
+  return promise
+    .then((response) => {
       console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch((error) => {
+      console.error('Got an error from the API');
+      return new Error();
     });
 }
+
+export default handleResponseFromAPI;
