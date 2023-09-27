243c5f4d2e112b2a6c5c618d395e0266f4591083
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-09-27 15:25:10 +0000

    Updates

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,6 +1,2 @@
 export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName: firstName,
-    lastName: lastName,
-  });
-}
+  return Promise.resolve({ firstName, lastName });
