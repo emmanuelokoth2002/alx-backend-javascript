99cc6486f60addba404852b25f96fa2825ff532b
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-09-27 15:31:35 +0000

    Updates

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,21 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
+/**
+ * Handles the profile signup process by calling signUpUser and uploadPhoto functions.
+ * @param {string} firstName - The first name of the user.
+ * @param {string} lastName - The last name of the user.
+ * @param {string} fileName - The file name for uploading a photo.
+ * @returns {Promise[]} An array of promises that represent the status and value of each operation.
+ */
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ];
+  const userPromise = signUpUser(firstName, lastName)
+    .then((response) => ({ status: 'fulfilled', value: response }))
+    .catch((error) => ({ status: 'rejected', value: error }));
 
-  return Promise.allSettled(promises).then(results => {
-    return results.map(result => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason.message,
-    }));
-  });
+  const photoPromise = uploadPhoto(fileName)
+    .then((response) => ({ status: 'fulfilled', value: response }))
+    .catch((error) => ({ status: 'rejected', value: error }));
+
+  return Promise.all([userPromise, photoPromise]);
 }
