59fdfdac0da4ace4fa428d2428a480e4ffd5e725
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-09-27 15:43:26 +0000

    Updates

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,20 +1,21 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
-export default async function asyncUploadUser() {
+async function asyncUploadUser() {
   try {
-    const [photoResponse, userResponse] = await Promise.all([
-      uploadPhoto(),
-      createUser(),
-    ]);
+    const photoResponse = await uploadPhoto('guillaume.jpg');
+    const userResponse = await createUser();
 
     return {
       photo: photoResponse,
       user: userResponse,
     };
   } catch (error) {
+    console.error('Error:', error);
     return {
       photo: null,
       user: null,
     };
   }
 }
+
+export default asyncUploadUser;
