105a397fca9bc6ad37043fbfee69c5045e38ae44
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-09-28 19:49:23 +0000

    Updates

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,21 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this._range = range;
+    this.range = range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  get range() {
+    return this.range;
   }
 
   cloneCar() {
-    const { brand, motor, color } = this;
-    return new Car(brand, motor, color);
+    const Clone = super.constructor[Symbol.species];
+    return new Clone();
   }
 }
