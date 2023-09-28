650126e943146487aff4196e0762165162185c04
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-09-28 15:11:46 +0000

    Updates

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,19 +1,17 @@
 // 5-building.js
 
-class Building {
+export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
   }
 
-  // Getter for 'sqft'
   get sqft() {
     return this._sqft;
   }
-
-  // Declare an abstract method
-  evacuationWarningMessage() {
-    throw new Error('Method evacuationWarningMessage must be implemented.');
-  }
 }
-
-export default Building;
