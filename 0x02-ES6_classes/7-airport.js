7058a0fd0f3b23d8b127a0c00445a4fa2c8c25c3
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-09-28 14:24:19 +0000

    Updates

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,4 +1,4 @@
-class Airport {
+export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
@@ -16,5 +16,3 @@ class Airport {
     return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
   }
 }
-
-export default Airport;
