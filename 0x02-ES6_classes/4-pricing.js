947059deac144ceb71209c8968781b560b926f01
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-09-28 14:40:18 +0000

    Updates

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,23 +1,23 @@
+// 4-pricing.js
+
 import Currency from './3-currency.js';
 
-export default class Pricing {
+class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
   }
 
+  // Getter and setter for 'amount'
   get amount() {
     return this._amount;
   }
 
   set amount(value) {
-    if (typeof value === 'number') {
-      this._amount = value;
-    } else {
-      throw new TypeError('Amount must be a number');
-    }
+    this._amount = value;
   }
 
+  // Getter and setter for 'currency'
   get currency() {
     return this._currency;
   }
@@ -26,19 +26,19 @@ export default class Pricing {
     if (value instanceof Currency) {
       this._currency = value;
     } else {
-      throw new TypeError('Currency must be an instance of Currency');
+      throw new TypeError('Currency must be an instance of Currency class');
     }
   }
 
+  // Method to display full price information
   displayFullPrice() {
     return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
+  // Static method to convert price
   static convertPrice(amount, conversionRate) {
-    if (typeof amount === 'number' && typeof conversionRate === 'number') {
-      return amount * conversionRate;
-    } else {
-      throw new TypeError('Amount and conversionRate must be numbers');
-    }
+    return amount * conversionRate;
   }
 }
+
+export default Pricing;
