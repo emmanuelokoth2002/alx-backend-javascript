135d84c4a696d93363f8c3222f10bc5abfe70376
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-09-28 14:13:38 +0000

    Updates

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,6 +1,6 @@
 import Currency from './3-currency.js';
 
-class Pricing {
+export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
@@ -10,16 +10,24 @@ class Pricing {
     return this._amount;
   }
 
-  set amount(newAmount) {
-    this._amount = newAmount;
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
   }
 
   get currency() {
     return this._currency;
   }
 
-  set currency(newCurrency) {
-    this._currency = newCurrency;
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
   }
 
   displayFullPrice() {
@@ -27,8 +35,10 @@ class Pricing {
   }
 
   static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    } else {
+      throw new TypeError('Amount and conversionRate must be numbers');
+    }
   }
 }
-
-export default Pricing;
