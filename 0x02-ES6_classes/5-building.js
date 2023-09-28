ac7665a30b25c294434d98d340e3cf584174a678
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-09-28 14:18:57 +0000

    Updates

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,4 +1,4 @@
-class Building {
+export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
   }
@@ -7,9 +7,8 @@ class Building {
     return this._sqft;
   }
 
+  // Abstract method to be overridden by subclasses
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
-
-export default Building;
