9c4e9786fa2b86820c1a092c688f7270fa5489e6
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-10-03 10:11:28 +0000

    Updates

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,12 +1,9 @@
 // 2-get_students_by_loc.js
 
 // Create a function that takes a list of students and a city as parameters
-function getStudentsByLocation(studentList, city) {
-  // Use the filter function to select students with the specified city
-  const studentsInCity = studentList.filter(student => student.location === city);
-
-  return studentsInCity;
+export default function getStudentsByLocation(ListStudents, city) {
+  if (Array.isArray(ListStudents) && (typeof city === 'string')) {
+    return ListStudents.filter((student) => student.location === city);
+  }
+  return [];
 }
-
-// Export the function
-export default getStudentsByLocation;
