8ce7a348208de8aad7bb6d0b7dcdcf9159290bd0
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-10-03 10:06:35 +0000

    Updates

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,17 +1,9 @@
 // 1-get_list_student_ids.js
 
 // Create a function that takes an array of objects as an argument
-function getListStudentIds(studentList) {
-  // Check if the argument is an array
-  if (!Array.isArray(studentList)) {
-    return [];
+export default function getListStudentIds(listStudents) {
+  if (Array.isArray(listStudents)) {
+    return listStudents.map((student) => student.id);
   }
-
-  // Use the map function to extract the 'id' property from each object
-  const ids = studentList.map(student => student.id);
-
-  return ids;
+  return [];
 }
-
-// Export the function
-export default getListStudentIds;
