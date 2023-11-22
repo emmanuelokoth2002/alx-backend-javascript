2fefb19124790ba9816b6ca26eb5231b3f3292fb
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-11-22 00:06:28 -0800

    Updates

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,23 +1,63 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const app = express();
 const port = 1245;
 
+// Middleware to parse the request body as JSON
+app.use(express.json());
+
+// Define routes
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  const databaseFile = process.argv[2];
+  const databaseFileName = process.argv[2];
 
-  countStudents(databaseFile)
-    .then(() => {
-      res.end();
-    })
-    .catch((error) => {
-      res.end(error.message);
-    });
+  if (!databaseFileName) {
+    res.status(500).send('Internal Server Error: Database file not provided.');
+    return;
+  }
+
+  fs.readFile(databaseFileName, 'utf-8', (err, data) => {
+    if (err) {
+      res.status(500).send('Internal Server Error: Unable to read database file.');
+      return;
+    }
+
+    const lines = data.trim().split('\n');
+    const students = [];
+    let csCount = 0;
+    let sweCount = 0;
+
+    for (const line of lines) {
+      const [name, age, field] = line.split(',');
+
+      if (name && age && field) {
+        students.push({ name, age, field });
+
+        if (field === 'CS') {
+          csCount++;
+        } else if (field === 'SWE') {
+          sweCount++;
+        }
+      }
+    }
+
+    let response = 'This is the list of our students\n';
+    response += `Number of students: ${students.length}\n`;
+    response += `Number of students in CS: ${csCount}. List: ${students
+      .filter((student) => student.field === 'CS')
+      .map((student) => student.name)
+      .join(', ')}\n`;
+    response += `Number of students in SWE: ${sweCount}. List: ${students
+      .filter((student) => student.field === 'SWE')
+      .map((student) => student.name)
+      .join(', ')}\n`;
+
+    res.send(response);
+  });
 });
 
 app.listen(port, () => {
