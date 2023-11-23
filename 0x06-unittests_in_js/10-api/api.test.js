1f01a7bffd50504f10abefa150dc108853e67d84
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-11-23 14:19:39 -0800

    Updates

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,56 +1,77 @@
 // 10-api/api.test.js
-const { expect } = require('chai');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 const request = require('request');
-const app = require('./api');
 
-describe('Index page', function () {
-  // Existing tests for the index page
+chai.use(chaiHttp);
 
-  it('Other?', function (done) {
-    // Your additional tests for the index page go here
-    done();
-  });
-});
+const expect = chai.expect;
 
-describe('Cart page', function () {
-  // Existing tests for the cart page
-
-  // Additional tests for the cart page go here
-});
-
-describe('Login endpoint', function () {
-  it('Correct status code and response for /login endpoint', function (done) {
-    request.post(
-      {
-        url: 'http://localhost:7865/login',
-        json: { userName: 'Alice' },
-      },
-      (error, response, body) => {
-        expect(response.statusCode).to.equal(200);
-        expect(body).to.equal('Welcome Alice');
-        done();
-      }
-    );
-  });
-
-  // Additional tests for the /login endpoint go here
-});
-
-describe('/available_payments endpoint', function () {
-  it('Correct status code and response for /available_payments endpoint', function (done) {
-    request.get('http://localhost:7865/available_payments', (error, response, body) => {
-      const expectedResponse = {
-        payment_methods: {
-          credit_cards: true,
-          paypal: false,
-        },
-      };
-
+describe('Index page', () => {
+  it('should return a status code of 200', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
-      expect(JSON.parse(body)).to.deep.equal(expectedResponse);
       done();
     });
   });
 
-  // Additional tests for the /available_payments endpoint go here
+  it('should return the message "Welcome to the payment system"', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
 });
+
+describe('Cart page', () => {
+  it('should return a status code of 200 when :id is a number', (done) => {
+    request('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return a status code of 404 when :id is NOT a number', (done) => {
+    request('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
+
+describe('/login endpoint', () => {
+  it('should return a status code of 200', (done) => {
+    chai
+      .request(app)
+      .post('/login')
+      .set('Content-Type', 'application/json')
+      .send({ userName: 'John' })
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('should return the correct welcome message', (done) => {
+    chai
+      .request(app)
+      .post('/login')
+      .set('Content-Type', 'application/json')
+      .send({ userName: 'Jane' })
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res.text).to.equal('Welcome Jane');
+        done();
+      });
+  });
+});
+
+describe('/available_payments endpoint', () => {
+  it('should return a status code of 200', (done) => {
+    chai.request(app).get('/available_payments').end((err, res) => {
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
